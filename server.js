ab3a1d892f40183a1f884a0433131c9175bd8691
David Carrera <carrera.david99@gmail.com>
2022-11-10 14:47:23 -0500

    fixes airpayTransaction

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const port = 3001;
+const port = 3002;
 
 // Server imports
 const express = require('express');
 const cors = require('cors');
 const app = express().use('*', cors());
 const bodyParser = require('body-parser');
-const { stringify } = require('querystring');
 const jsonParser = bodyParser.json();
 app.use(express.json());
 app.use(jsonParser);
 var axios = require("axios").default;
 
 // Aptos Imports
+const aptos = require('aptos');
 const aptosWeb3 = require('@martiandao/aptos-web3-bip44.js');
-const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
-const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+const { bcsSerializeUint64 } = require('@martiandao/aptos-web3-bip44.js/dist/bcs');
+const NODE_URL = "https://fullnode.testnet.aptoslabs.com";
+const FAUCET_URL = "https://faucet.testnet.aptoslabs.com";
 const walletClient = new aptosWeb3.WalletClient(NODE_URL, FAUCET_URL);
 const client = new aptosWeb3.AptosClient(NODE_URL);
+
 
 // ------ Constants ------
 const ACCEPT = 200;
 const REJECT = 402;
+const ACCOUNT_PRIVATE_KEY = '0x5ef3664d7687b26acc5a4ba76291f993b23956e2d819fe6439640cbb0b37c4f5';
 // ------ Constants ------
+
 
 // ------ Helper functions ------
 async function getMartianAccountsAptosAmount(mnemonic) {
@@ -41,75 +45,39 @@ async function getMartianAccountsAptosAmount(mnemonic) {
     // console.log('Balance:', await walletClient.getBalance(address));
 }
 
-async function aptosBlockchainTransaction(user_address, public_key, signature, transaction_amount){
+async function runAptosBlockchainTransaction(transaction_amount, ACCOUNT_PRIVATE_KEY){
+    const client = new aptos.AptosClient(NODE_URL);
+    const sender = new aptos.AptosAccount(aptos.HexString.ensure(ACCOUNT_PRIVATE_KEY).toUint8Array());
 
-    // estimate_gas_price = estimateGasPrice()
-    // estimated_total_transaction_amount = transaction_amount + estimate_gas_price
+    const payload = new aptos.TxnBuilderTypes.TransactionPayloadEntryFunction(
+        aptos.TxnBuilderTypes.EntryFunction.natural(
+            "0x7899cc9c5b8ef15605ef46adc52d004db3caa5e4b2bb64da46cb7b9363f8e934::router", // Address and module/script name
+            "deposit_to_vault", // Function
+            [], // Argument types (optional?)
+            [bcsSerializeUint64(100000)] // Arguments
+        )
+    );
 
-    gas_amount = 10
+    console.log("Built payload");
 
-    var options = {
-        method: 'POST',
-        url: 'https://fullnode.devnet.aptoslabs.com/v1/transactions',
-        headers: {'Content-Type': 'application/json'},
-        data: {
-          sender: user_address,
+    // console.log(sender.address());
+    // return;
 
-          // What should these 4 be?
-          sequence_number: '32425224034', // see getAccount on rpc docs
-          max_gas_amount: stringify(gas_amount),
-          gas_unit_price: stringify(gas_amount),
-          expiration_timestamp_secs: '32425224034',
+    const raw = await client.generateRawTransaction(sender.address(), payload);
 
-          // Change function to correct move module
-          payload: {
-            type: 'entry_function_payload',
-            function: '0x1::aptos_coin::transfer',
+    console.log("Built raw transaction");
 
-            // what types can be put in?
-            type_arguments: ['string'],
-            arguments: [transaction_amount, gas_amount]
-          },
+    const signed = aptos.AptosClient.generateBCSTransaction(sender, raw);
 
-          // How to get a users signature
-          signature: {
-            type: 'ed25519_signature',
-            public_key: public_key,
-            signature: '0x88fbd33f54e1126269769780feb24480428179f552e2313fbe571b72e62a1ca1'
-          }
-        }
-    };
+    console.log("Built signed transaction " + signed);
 
-    response_status = REJECT
-    axios.request(options).then(function (response) {
-        response_status = ACCEPT;
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
+    const response = await client.submitSignedBCSTransaction(signed);
 
-    return response_status;
+    console.log(`Response: ${response.body}`);
+
+    // May not be needed?
+    await client.waitForTransaction(response.hash);
 }
-
-// async function estimateGasPrice(){
-//     estimate_gas_price = 0
-
-//     var options = {
-//         method: 'GET',
-//         url: 'https://fullnode.devnet.aptoslabs.com/v1/estimate_gas_price',
-//         headers: {'Content-Type': 'application/json'}
-//       };
-
-//       axios.request(options).then(function (response) {
-//         estimate_gas_price = response.data.gas_estimate
-
-//         console.log(response.data.gas_estimate);
-//       }).catch(function (error) {
-//         console.error(error);
-//       });
-
-//       return estimate_gas_price
-// }
 // ------ Helper functions ------
 
 
@@ -165,48 +133,10 @@ app.get('/getCard', async (req, res) => {
 
 app.post("/airpayTransaction", async (req, res) => {
 
-    // To do:
-        // Create webhook st. only JIT funding requests go through, not notifications etc.
-
-    // Pseudo:
-        // Check format of request before executing logic. If improper request, error
-        // Call move logic
-        // If move logic fails, error
-        // If move logic succeeds, 200 + return jit funding response
-
-    // const data = req.body;
-    // console.log(data);
-    // return;
-
-    // Fields hard-coded in request for testing purposes
-    // user_address = data.user_address;
-    // public_key = data.public_key;
-    // signature = data.signature;
-    // transaction_amount = data.transaction_amount;
-
-    // response_status = await aptosBlockchainTransaction(user_address, public_key, signature, transaction_amount);
-    // console.log(response_status);
-    // response_status = ACCEPT;
-
-    // const jsonResponse = {
-    //     "token": data.token,
-    //     "method": "pgfs.authorization",
-    //     "user_token": data.user_token,
-    //     "amount": data.gpa_order.jit_funding.amount,
-    //     "original_jit_funding_token": data.gpa_order.jit_funding.token
-    // }
-
-    // console.log(jsonResponse);
+    const data = req.body;
+    console.log(data);
     
-    // res.status(response_status).send({
-    //     "jit_funding": {
-    //         "token": data.token,
-    //         "method": "pgfs.authorization",
-    //         "user_token": data.user_token,
-    //         "amount": data.gpa_order.jit_funding.amount,
-    //         "original_jit_funding_token": data.gpa_order.jit_funding.token
-    //     }
-    // });
+    await runAptosBlockchainTransaction(100000, ACCOUNT_PRIVATE_KEY);
 
     const json_response = {
         "category": "Purchase",
@@ -214,11 +144,12 @@ app.post("/airpayTransaction", async (req, res) => {
         "amount_paid_usd": "10.67",
         "swap_price_impact": "0.1",
         "fees": "0.3"
-    }
+    };
 
     console.log(json_response);
     res.status(200).send(json_response);
 });
 // ------ Endpoints ------
+
 
 app.listen(process.env.PORT || port, () => console.log(`Express app listening at http::/localhost:${port}`));
